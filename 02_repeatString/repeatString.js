1240a00056c37eb6de541b4a834cdc0fed0df328
Roualeyn <roualeyn@gmail.com>
2022-08-23 20:34:18 +0200

    Complete Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    let result = "";
+    for (let i = 0; i < num; i++){
+        result = result + string;
+    }
+    return result;
 };
 
+repeatString("hyey", 2);
 // Do not edit below this line
 module.exports = repeatString;
