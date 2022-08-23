d6308e2a0e7318605f430c71e2a8cc680e9e2050
Roualeyn <roualeyn@gmail.com>
2022-08-23 21:20:26 +0200

    Complete Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...removedArray) {
+    for (const i of removedArray) {
+        let index = inputArray.indexOf(i);
+        if (index >= 0) {
+            inputArray.splice(index, 1);
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
