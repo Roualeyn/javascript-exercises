e5665917eb798302f8a0c053f035bec3d8e5f8da
Roualeyn <roualeyn@gmail.com>
2022-09-02 00:04:00 +0200

    Complete Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        let oldestAge = -oldest["yearOfBirth"];
+        let personAge = -person["yearOfBirth"];
 
+        oldestAge += (oldest["yearOfDeath"] === undefined) ? new Date().getFullYear() : oldest["yearOfDeath"];        
+        personAge += (person["yearOfDeath"] === undefined) ? new Date().getFullYear() : person["yearOfDeath"];
+        
+        return (oldestAge < personAge) ? person : oldest;
+    }, people[0]);
 };
 
 // Do not edit below this line
