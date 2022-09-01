a463cf3793a676ce9304ac52b09f2222f40ec9d0
Roualeyn <roualeyn@gmail.com>
2022-09-01 22:25:20 +0200

    Complete Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+	return inputArray.reduce(add, 0);
+}
+
+const multiply = function(inputArray) {
+  return inputArray.reduce((total, current) => total * current, 1);  
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+	return a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+  result = 1;
+  for (let i = 1; i <= num; i++) {
+      result *= i;
+  }
+  return result;	
 };
 
 // Do not edit below this line
